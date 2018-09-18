f09eebbfaa52572192a0d08f36bbb178378d6802
Nathaniel Jones <nathan_geology@utexas.edu>
2018-09-18 13:10:54 +0200

    slight revamp, working on a pyqt turking tool real quick

--- core_photo_force/scripts_modules/pyqt_turking.py
+++ core_photo_force/scripts_modules/pyqt_turking.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QScrollArea, QVBoxLayout
+import PyQt5.Qt
+from PyQt5.QtGui import QIcon, QPixmap
 
 
 class App(QWidget):
@@ -12,16 +13,38 @@ class App(QWidget):
         self.top = 10
         self.width = 640
         self.height = 480
+        self.scrollarea = None
+        self.scroll = None
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+        self.scrollarea = QScrollArea(parent=self)
+       
         # Create widget
-        label = QLabel(self)
-        pixmap = QPixmap('image.jpeg')
+
+        #   Scroll Area Properties
+
+        label = QLabel(self.scrollarea)
+        # scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)
+        # scroll.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.scrollarea.setWidgetResizable(True)
+
+
+        #   Scroll Area Layer add
+        scroll_layout = QVBoxLayout(self)
+
+        scroll_layout.setContentsMargins(0, 0, 0, 0)
+        self.setLayout(scroll_layout)
+
+        pixmap = QPixmap('/Users/nathanieljones/PycharmProjects/Force-Hackathon-Grain-Size/core_photo_force/data/well_6507_7_4/6507_7_4_24345_24366.jpg')
         label.setPixmap(pixmap)
-        self.resize(pixmap.width(), pixmap.height())
+
+        #self.resize(pixmap.width(), pixmap.height())
+
+
+
         self.show()
 
 
