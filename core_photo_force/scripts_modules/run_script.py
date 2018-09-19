ee54bddad4ae6c061e2acd568f475e3d4de7d896
Nathaniel Jones <nathan_geology@utexas.edu>
2018-09-19 10:53:30 +0200

    got the sed structures and sand models done

--- core_photo_force/scripts_modules/run_script.py
+++ core_photo_force/scripts_modules/run_script.py
@@ -1,4 +1,12 @@
 from core_photo_force.scripts_modules.well_core import stitch_images
-
+import  pickle
 if __name__ == '__main__':
+    objects = []
+    with open('/Volumes/Samsung_T5/Hackathon/Force-Hackathon-Grain-Size/feature_table_peter.pkl', 'rb') as f:
+        while True:
+            try:
+                objects.append(pickle.load(f))
+            except EOFError:
+                break
+    feature_table = objects[0]
     stitch_images()
